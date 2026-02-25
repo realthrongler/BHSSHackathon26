8d3040d82190fafc4cce6903372279f8ebe6a862
Derek Tung <derektung1925@gmail.com>
2026-02-25 14:15:14 -0500

    burger murger
    we will prevail

--- src/HomeScreen1.java
+++ src/HomeScreen1.java
@@ -35,38 +35,51 @@ public class HomeScreen1 extends javax.swing.JFrame {
         drop_minutes = new javax.swing.JComboBox<>();
         btn_add = new javax.swing.JButton();
         txt_eventName = new javax.swing.JTextField();
-        jSeparator1 = new javax.swing.JSeparator();
         txt_itemsNeeded = new javax.swing.JTextField();
         lbl_money = new javax.swing.JLabel();
         txt_moneyNeeded = new javax.swing.JTextField();
         lbl_pending = new javax.swing.JLabel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        list_pending = new javax.swing.JList<>();
-        btn_decline = new javax.swing.JButton();
-        btn_accept = new javax.swing.JButton();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
+        jPanel1.setBackground(new java.awt.Color(240, 140, 40));
         jPanel1.setMaximumSize(new java.awt.Dimension(270, 380));
         jPanel1.setMinimumSize(new java.awt.Dimension(270, 380));
         jPanel1.setLayout(null);
 
+        drop_month.setBackground(new java.awt.Color(255, 255, 255));
+        drop_month.setForeground(new java.awt.Color(0, 0, 0));
         drop_month.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" }));
         jPanel1.add(drop_month);
-        drop_month.setBounds(54, 17, 96, 26);
+        drop_month.setBounds(90, 120, 95, 26);
 
+        drop_hour.setBackground(new java.awt.Color(255, 255, 255));
+        drop_hour.setForeground(new java.awt.Color(0, 0, 0));
         drop_hour.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12" }));
         jPanel1.add(drop_hour);
-        drop_hour.setBounds(54, 45, 92, 26);
+        drop_hour.setBounds(90, 150, 92, 26);
 
+        drop_day.setBackground(new java.awt.Color(255, 255, 255));
+        drop_day.setForeground(new java.awt.Color(0, 0, 0));
         drop_day.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25", "26", "27", "28", "29", "30", "31" }));
+        drop_day.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                drop_dayActionPerformed(evt);
+            }
+        });
         jPanel1.add(drop_day);
-        drop_day.setBounds(152, 17, 76, 26);
+        drop_day.setBounds(200, 120, 76, 26);
 
+        drop_minutes.setBackground(new java.awt.Color(255, 255, 255));
+        drop_minutes.setForeground(new java.awt.Color(0, 0, 0));
         drop_minutes.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { ":00", ":15", ":30", ":45" }));
         jPanel1.add(drop_minutes);
-        drop_minutes.setBounds(152, 45, 76, 26);
+        drop_minutes.setBounds(200, 150, 76, 26);
 
+        btn_add.setBackground(new java.awt.Color(210, 110, 10));
+        btn_add.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        btn_add.setForeground(new java.awt.Color(255, 255, 255));
         btn_add.setText("Add to schedule");
         btn_add.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -74,71 +87,76 @@ public class HomeScreen1 extends javax.swing.JFrame {
             }
         });
         jPanel1.add(btn_add);
-        btn_add.setBounds(80, 180, 120, 27);
+        btn_add.setBounds(110, 330, 140, 27);
 
+        txt_eventName.setBackground(new java.awt.Color(255, 255, 255));
+        txt_eventName.setForeground(new java.awt.Color(0, 0, 0));
         txt_eventName.setText("Event Title");
         jPanel1.add(txt_eventName);
-        txt_eventName.setBounds(54, 91, 169, 26);
-        jPanel1.add(jSeparator1);
-        jSeparator1.setBounds(0, 75, 270, 10);
+        txt_eventName.setBounds(100, 210, 169, 26);
 
+        txt_itemsNeeded.setBackground(new java.awt.Color(255, 255, 255));
+        txt_itemsNeeded.setForeground(new java.awt.Color(0, 0, 0));
         txt_itemsNeeded.setText("Items Required");
         jPanel1.add(txt_itemsNeeded);
-        txt_itemsNeeded.setBounds(54, 150, 169, 26);
+        txt_itemsNeeded.setBounds(100, 290, 169, 26);
 
         lbl_money.setFont(new java.awt.Font("Consolas", 0, 12)); // NOI18N
+        lbl_money.setForeground(new java.awt.Color(255, 255, 255));
         lbl_money.setText("$");
         jPanel1.add(lbl_money);
-        lbl_money.setBounds(42, 126, 6, 15);
+        lbl_money.setBounds(90, 260, 7, 15);
 
+        txt_moneyNeeded.setBackground(new java.awt.Color(255, 255, 255));
+        txt_moneyNeeded.setForeground(new java.awt.Color(0, 0, 0));
         txt_moneyNeeded.setText("Money Required");
         jPanel1.add(txt_moneyNeeded);
-        txt_moneyNeeded.setBounds(54, 119, 169, 26);
+        txt_moneyNeeded.setBounds(100, 250, 169, 26);
 
+        lbl_pending.setFont(new java.awt.Font("Segoe UI", 1, 24)); // NOI18N
+        lbl_pending.setForeground(new java.awt.Color(255, 255, 255));
         lbl_pending.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        lbl_pending.setText("Pending hangouts:");
+        lbl_pending.setText("Create New Event");
         jPanel1.add(lbl_pending);
-        lbl_pending.setBounds(60, 220, 160, 16);
+        lbl_pending.setBounds(80, 50, 210, 30);
 
-        jScrollPane2.setViewportView(list_pending);
-
-        jPanel1.add(jScrollPane2);
-        jScrollPane2.setBounds(10, 240, 250, 100);
-
-        btn_decline.setText("Decline");
-        jPanel1.add(btn_decline);
-        btn_decline.setBounds(30, 350, 76, 27);
-
-        btn_accept.setText("Accept");
-        btn_accept.addActionListener(new java.awt.event.ActionListener() {
+        jButton1.setBackground(new java.awt.Color(210, 110, 10));
+        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 12)); // NOI18N
+        jButton1.setForeground(new java.awt.Color(255, 255, 255));
+        jButton1.setText("Home");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                btn_acceptActionPerformed(evt);
+                jButton1ActionPerformed(evt);
             }
         });
-        jPanel1.add(btn_accept);
-        btn_accept.setBounds(160, 350, 76, 27);
+        jPanel1.add(jButton1);
+        jButton1.setBounds(20, 10, 76, 27);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 363, Short.MAX_VALUE)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, 481, Short.MAX_VALUE)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void btn_acceptActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_acceptActionPerformed
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
         // TODO add your handling code here:
-    }//GEN-LAST:event_btn_acceptActionPerformed
+    }//GEN-LAST:event_jButton1ActionPerformed
 
     private void btn_addActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btn_addActionPerformed
         // TODO add your handling code here:
     }//GEN-LAST:event_btn_addActionPerformed
+
+    private void drop_dayActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_drop_dayActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_drop_dayActionPerformed
 
     /**
      * @param args the command line arguments
@@ -166,19 +184,15 @@ public class HomeScreen1 extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton btn_accept;
     private javax.swing.JButton btn_add;
-    private javax.swing.JButton btn_decline;
     private javax.swing.JComboBox<String> drop_day;
     private javax.swing.JComboBox<String> drop_hour;
     private javax.swing.JComboBox<String> drop_minutes;
     private javax.swing.JComboBox<String> drop_month;
+    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JScrollPane jScrollPane2;
-    private javax.swing.JSeparator jSeparator1;
     private javax.swing.JLabel lbl_money;
     private javax.swing.JLabel lbl_pending;
-    private javax.swing.JList<String> list_pending;
     private javax.swing.JTextField txt_eventName;
     private javax.swing.JTextField txt_itemsNeeded;
     private javax.swing.JTextField txt_moneyNeeded;
